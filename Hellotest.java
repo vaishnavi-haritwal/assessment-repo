714abfb6521cff192d6611d132147ab0c01c93a8
vaishnavi <vharitwal@lnw.com>
2022-10-28 15:32:27 +0530

    inital commit

--- Hellotest.java
+++ Hellotest.java
@@ -1,7 +1,7 @@
 public class Hellotest{
         public static void main(String[] args)
         {
-                 System.out.println("Welcome to the world of github");
+                 System.out.println("Welcome to the world of gitlab");
         }
 }
 
